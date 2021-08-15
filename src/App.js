548d2364721e82be500f00c1bd320b6fef9588a0
StanfordDC <stanforddchandra@gmail.com>
2021-08-15 20:10:39 +0800

    Update App.js

--- src/App.js
+++ src/App.js
@@ -1,9 +1,14 @@
 import './App.css';
+import { Header } from './components/Header';
+import { Balance } from './components/Balance';
 
 function App() {
   return (
     <div>
-      Hello world
+      <Header />
+      <div className="container">
+        <Balance />
+      </div>
     </div>
   );
 }
